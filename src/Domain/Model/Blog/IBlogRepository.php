6117cee2a840ef78e42fc007c07bc45e6f9bcf21
hirano-ta <hirano-ta@ibjapan.jp>
2025-07-10 18:36:50 +0900

    BlogRepositoryのインターフェーズを実装

---
+++ src/Domain/Model/Blog/IBlogRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Domain\Model\Blog;
+
+use Domain\Model\Blog\Blog;
+use Domain\Model\Blog\BlogId\BlogId;
+
+interface IBlogRepository
+{
+  public function save(Blog $blog): void;
+
+  public function update(Blog $blog): void;
+
+  public function delete(Blog $blog): void;
+
+  public function findById(BlogId $id): ?Blog;
+
+  public function findAll(); 
+}
