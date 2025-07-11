b13ca3b04990eefaa07da3fa571936e95637eb4c
hirano-ta <hirano-ta@ibjapan.jp>
2025-07-11 18:30:19 +0900

    BlogRepositoryの仮実装

---
+++ src/Infrastructures/Repositories/BlogRepository.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Infrastructures\Repositories;
+
+use Domain\Model\Blog\IBlogRepository;
+use Domain\Model\Blog\Blog;
+use Domain\Model\Blog\BlogId\BlogId;
+use Domain\Model\Blog\Title\Title;
+use Domain\Model\Blog\Body\Body;
+use Domain\Model\Author\AuthorId\AuthorId;
+use Doctrine\DBAL\Connection;
+use Config\DbConnection;
+
+class BlogRepository implements IBlogRepository
+{
+  private Connection $connection;
+
+  public function __construct()
+  {
+    $this->connection = DbConnection::getConnection();
+  }
+
+  public function save(Blog $blog): void
+  {
+    $this->connection->insert('blogs', [
+      'id' => $blog->getId()->getValue(),
+      'title' => $blog->getTitle()->getValue(),
+      'body' => $blog->getBody()->getValue(),
+      'author_id' => $blog->getAuthorId()->getValue(),
+    ]);
+  }
+
+  public function update(Blog $blog): void
+  {
+    $this->connection->update('blogs', [
+      'title' => $blog->getTitle()->getValue(),
+      'body' => $blog->getBody()->getValue(),
+    ], ['id' => $blog->getId()->getValue()]);
+  }
+
+  public function delete(Blog $blog): void
+  {
+    $this->connection->delete('blogs', ['id' => $blog->getId()->getValue()]);
+  }
+
+  public function findById(BlogId $id): ?Blog
+  {
+    $sql = 'SELECT * FROM blogs WHERE id = :id';
+    $row = $this->connection->fetchAssociative($sql, ['id' => $id->getValue()]);
+    if (!$row) {
+      return null;
+    }
+    return new Blog(
+      new BlogId($row['id']),
+      new Title($row['title']),
+      new Body($row['body']),
+      new AuthorId($row['author_id']),
+    );
+  }
+
+  public function findAll()
+  {
+    $sql = 'SELECT * FROM blogs';
+    $rows = $this->connection->fetchAllAssociative($sql);
+    return array_map(function ($row) {
+      return new Blog(
+        new BlogId($row['id']),
+        new Title($row['title']),
+        new Body($row['body']),
+        new AuthorId($row['author_id']),
+      );
+    }, $rows);
+  }
+}
